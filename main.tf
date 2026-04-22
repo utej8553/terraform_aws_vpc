9cf18031f2c0c2e9dd5a7fd81606b8d519b6054a
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-22 19:38:12 +0530

    added public subnet

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "aws_vpc" "main"{
 resource "aws_internet_gateway" "igw"{
   vpc_id = aws_vpc.main.id
 }
+resource "aws_subnet" "public"{
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
