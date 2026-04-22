0f82b059faea8e27816609bfec97cef927b4010e
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-22 19:49:14 +0530

    modified variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,13 @@
 variable "region" {
+  type = string
   default = "ap-south-1"
 }
 
 variable "public_key_path" {
-  description = "Path to your public SSH key"
+  description = "Path "
+}
+
+variable "ami" {
+  type = string
+  default = "ami-07062e2a343acc423"
 }
