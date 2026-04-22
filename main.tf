9836c6c2bc422678520b367c37967dd2daf8db42
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-22 19:44:10 +0530

    added bastian sg

--- main.tf
+++ main.tf
@@ -45,3 +45,19 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
 }
+resource "aws_security_group" "bastian_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
