cabb9a4c280287e5eac4f12807e001a8ba9f5853
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-22 19:46:43 +0530

    added bastian host

--- main.tf
+++ main.tf
@@ -65,3 +65,13 @@ resource "aws_key_pair" "key" {
   key_name = "bastain-key"
   public_key = file(var.public_key_path)
 }
+resource "aws_instance" "bastain"{
+  ami = var.ami
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.bastian_sg.id]
+  key_name = aws_key_pair.key.key_name
+  tags = {
+    Name = "bastain-host"
+  }
+}
