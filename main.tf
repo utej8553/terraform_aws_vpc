806ad240eed9b48d132e0e48a2b601eee40adfe9
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-22 19:41:23 +0530

    added internet access

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+}
 provider "aws"{
   region = var.region
 }
@@ -27,4 +35,9 @@ resource "aws_subnet" "private"{
 }
 resource "aws_route_table" "public_rt"{
   vpc_id = aws_vpc.main.id
+}
+resource "aws_route" "internet_access" {
+  route_table_id = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
 }
